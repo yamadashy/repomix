7dc03a2e1084d252f995c8668030746a9bbe8239
Kazuki Yamada <koukun0120@gmail.com>
2025-11-09 18:37:13 +0900

    chore(deps): Upgrade @repomix/tree-sitter-wasms to 0.1.15
    Updated @repomix/tree-sitter-wasms from 0.1.14 to 0.1.15 and fixed C# query
    compatibility issues. The new version of tree-sitter-c-sharp changed several
    node names and field names, requiring simplification of the C# query pattern.
    
    Changes:
    - Upgraded @repomix/tree-sitter-wasms to 0.1.15
    - Removed incompatible query patterns from queryCSharp.ts that used deprecated
      field names ('bases', 'target') and node names ('type_constraint')
    - Simplified C# query to focus on core definitions (classes, interfaces,
      methods, namespaces) and basic references
    
    All tests pass with the updated version.

--- src/core/treeSitter/queries/queryCSharp.ts
+++ src/core/treeSitter/queries/queryCSharp.ts
@@ -5,37 +5,13 @@ export const queryCSharp = `
   name: (identifier) @name.definition.class
 ) @definition.class
 
-(class_declaration
-  bases: (base_list (_) @name.reference.class)
-) @reference.class
-
 (interface_declaration
   name: (identifier) @name.definition.interface
 ) @definition.interface
 
-(interface_declaration
-  bases: (base_list (_) @name.reference.interface)
-) @reference.interface
-
 (method_declaration
   name: (identifier) @name.definition.method
 ) @definition.method
-
-(object_creation_expression
-  type: (identifier) @name.reference.class
-) @reference.class
-
-(type_parameter_constraints_clause
-  target: (identifier) @name.reference.class
-) @reference.class
-
-(type_constraint
-  type: (identifier) @name.reference.class
-) @reference.class
-
-(variable_declaration
-  type: (identifier) @name.reference.class
-) @reference.class
 
 (invocation_expression
   function:
